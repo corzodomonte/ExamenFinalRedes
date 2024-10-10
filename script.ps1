e4bcd98f526c5db46ee4dd34aaf99b9190b39442
Tinokos <48957616+Tinokos@users.noreply.github.com>
2024-10-10 10:28:08 +0200

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,4 +1,5 @@
-﻿$servers= @("10.57.401.196","google.com","4.4.4.4")
+#Que guay el script.﻿
+$servers= @("10.57.401.196","google.com","4.4.4.4")
 foreach ($server in $servers) { 
 Test-Connection -ComputerName $server -Count 2
 }
